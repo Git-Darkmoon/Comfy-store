90861ec54c1f5e416b48ac5187737e26139dc5fd
Nicolas Mayorga <namayorgav@gmail.com>
2023-12-13 15:20:45 -0500

    feat: Landing components updated.

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,9 +1,19 @@
-import { Hero } from "../components"
+import { FeaturedProducts, Hero } from "../components"
+import { customFetch } from "../utils"
+
+const url = "/products?featured=true"
+
+export const loader = async () => {
+  const resp = await customFetch(url)
+  const products = resp.data.data
+  return { products }
+}
 
 function Landing() {
   return (
     <>
       <Hero />
+      <FeaturedProducts />
     </>
   )
 }
