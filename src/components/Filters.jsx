489ecb8fadfd6a145b8863442aedea4ad867830a
Nicolas Mayorga <namayorgav@gmail.com>
2023-12-17 12:09:29 -0500

    feat: select inputs filters.

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,7 +1,10 @@
-import { Form, Link } from "react-router-dom"
+import { Form, Link, useLoaderData } from "react-router-dom"
 import FormInput from "./FormInput"
+import FormSelect from "./FormSelect"
 
 function Filters() {
+  const { meta } = useLoaderData()
+
   return (
     <Form className="bg-base-200 rounded-md px-8 py-4 grid gap-x-4 gap-y-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center">
       {/* SEARCH */}
@@ -11,11 +14,34 @@ function Filters() {
         name={"search"}
         size={"input-sm"}
       />
+      {/* CATEGORIES */}
+      <FormSelect
+        label={"select category"}
+        name={"category"}
+        list={meta.categories}
+        size={"select-sm"}
+      />
+      {/* COMPANIES */}
+      <FormSelect
+        label={"select company"}
+        name={"company"}
+        list={meta.companies}
+        size={"select-sm"}
+      />
+      {/* ORDER */}
+      <FormSelect
+        label={"sort by"}
+        name={"order"}
+        list={["A-Z", "Z-A", "high", "low"]}
+        size={"select-sm"}
+      />
       {/* BUTTONS */}
       <button type="submit" className="btn btn-primary btn-sm">
         Search
       </button>
-      <Link to="/products" className="btn btn-accent btn-sm"></Link>
+      <Link to="/products" className="btn btn-accent btn-sm">
+        Reset
+      </Link>
     </Form>
   )
 }
