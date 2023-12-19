51566e4c98ac95c0d3cc002cb55ac2f9744968f1
Nicolas Mayorga <namayorgav@gmail.com>
2023-12-19 09:28:58 -0500

    styles: changed light theme color palette.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,6 +7,6 @@ export default {
   },
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
   daisyui: {
-    themes: ["garden", "dracula"],
+    themes: ["autumn", "dracula"],
   },
 }
