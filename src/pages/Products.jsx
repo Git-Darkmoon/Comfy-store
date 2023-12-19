2ba3f4e1d64912c29e6b39e023f40733b8bb9d44
Nicolas Mayorga <namayorgav@gmail.com>
2023-12-19 09:29:43 -0500

    feat: URL query params obtained as an object.

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -4,9 +4,10 @@ import { customFetch } from "../utils"
 const url = "/products"
 
 export const loader = async ({ request }) => {
-  const params = new URL(request.url).searchParams
-  const search = params.get("search")
-  console.log(search)
+  const params = Object.fromEntries([
+    ...new URL(request.url).searchParams.entries(),
+  ])
+  console.log(params)
   const response = await customFetch(url)
   const products = response.data.data
   const meta = response.data.meta
