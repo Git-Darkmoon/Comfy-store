eac9720263767443e950adbeda7579bd5268d4c0
Nicolas Mayorga <namayorgav@gmail.com>
2023-12-18 21:47:53 -0500

    chore: URL params.

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -4,6 +4,9 @@ import { customFetch } from "../utils"
 const url = "/products"
 
 export const loader = async ({ request }) => {
+  const params = new URL(request.url).searchParams
+  const search = params.get("search")
+  console.log(search)
   const response = await customFetch(url)
   const products = response.data.data
   const meta = response.data.meta
