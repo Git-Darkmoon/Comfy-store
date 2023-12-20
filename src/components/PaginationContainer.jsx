76238e7997883d1dac414a495095a0019259a948
Nicolas Mayorga <namayorgav@gmail.com>
2023-12-20 11:25:01 -0500

    feat: pagination at the end of the products

--- src/components/PaginationContainer.jsx
+++ src/components/PaginationContainer.jsx
@@ -1,4 +1,49 @@
+import { useLoaderData } from "react-router-dom"
+
 function PaginationContainer() {
-  return <div>PaginationContainer</div>
+  const { meta } = useLoaderData()
+  const { pageCount, page } = meta.pagination
+
+  const pages = Array.from({ length: pageCount }, (_, index) => {
+    return index + 1
+  })
+
+  const handlePageChange = (pageNumber) => {
+    console.log(pageNumber)
+  }
+
+  if (pageCount < 2) return null
+
+  return (
+    <div className="mt-16 flex justify-end">
+      <div className="join">
+        <button
+          className="btn btn-xs sm:btn-md join-item"
+          onClick={() => handlePageChange("prev")}
+        >
+          Prev
+        </button>
+        {pages.map((pageNum) => {
+          return (
+            <button
+              key={pageNum}
+              onClick={() => handlePageChange(pageNum)}
+              className={`btn btn-xs sm:btn-md border-none join-item ${
+                pageNum === page ? "bg-base-300 border-base-300" : ""
+              }`}
+            >
+              {pageNum}
+            </button>
+          )
+        })}
+        <button
+          className="btn btn-xs sm:btn-md join-item"
+          onClick={() => handlePageChange("next")}
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  )
 }
 export default PaginationContainer
