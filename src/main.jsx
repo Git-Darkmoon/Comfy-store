151729abb16d77a36df53255bf4b95a63726c3b5
Nicolas Mayorga <namayorgav@gmail.com>
2023-12-24 09:27:56 -0500

    feat: Global toaster.

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,13 @@ import ReactDOM from "react-dom/client"
 import App from "./App.jsx"
 import "./index.css"
 
-import "react-toastify/dist/ReactToastify.css"
-import { ToastContainer } from "react-toastify"
 import { store } from "./store.js"
 import { Provider } from "react-redux"
+import { Toaster } from "sonner"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <App />
-    <ToastContainer position="top-center" />
+    <Toaster position="top-right" closeButton />
   </Provider>
 )
