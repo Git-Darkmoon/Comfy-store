ca9791e895dbf26a5afd0bae9484210e762e1c91
Nicolas Mayorga <namayorgav@gmail.com>
2023-12-26 17:58:39 -0500

    feat: React Query approach to featured products.

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -3,16 +3,18 @@ import { customFetch } from "../utils"
 
 const url = "/products"
 
-export const loader = async ({ request }) => {
-  const queryParams = Object.fromEntries([
-    ...new URL(request.url).searchParams.entries(),
-  ])
-  // console.log(params)
-  const response = await customFetch(url, { params: queryParams })
-  const products = response.data.data
-  const meta = response.data.meta
-  return { products, meta, queryParams }
-}
+export const loader =
+  (queryClient) =>
+  async ({ request }) => {
+    const queryParams = Object.fromEntries([
+      ...new URL(request.url).searchParams.entries(),
+    ])
+    // console.log(params)
+    const response = await customFetch(url, { params: queryParams })
+    const products = response.data.data
+    const meta = response.data.meta
+    return { products, meta, queryParams }
+  }
 
 function Products() {
   return (
