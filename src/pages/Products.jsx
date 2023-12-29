d6f3cda6a83f3ff59ee6cbd3995ece0dd96b79ef
Nicolas Mayorga <namayorgav@gmail.com>
2023-12-28 19:03:02 -0500

    feat: React Query caching data for requests.

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -3,6 +3,27 @@ import { customFetch } from "../utils"
 
 const url = "/products"
 
+const allProductsQuery = (queryParams) => {
+  const { search, category, company, sort, price, shipping, page } = queryParams
+
+  return {
+    queryKey: [
+      "products",
+      search ?? "",
+      category ?? "all",
+      company ?? "all",
+      sort ?? "a-z",
+      price ?? 100000,
+      shipping ?? false,
+      page ?? 1,
+    ],
+    queryFn: () =>
+      customFetch(url, {
+        params: queryParams,
+      }),
+  }
+}
+
 export const loader =
   (queryClient) =>
   async ({ request }) => {
@@ -10,7 +31,9 @@ export const loader =
       ...new URL(request.url).searchParams.entries(),
     ])
     // console.log(params)
-    const response = await customFetch(url, { params: queryParams })
+    const response = await queryClient.ensureQueryData(
+      allProductsQuery(queryParams)
+    )
     const products = response.data.data
     const meta = response.data.meta
     return { products, meta, queryParams }
